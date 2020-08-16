235ab521ef0ffe4dff650ce2b3b9a8f6f64214b0
santhooosh <55274708+santhooosh@users.noreply.github.com>
2020-08-16 23:18:58 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,90 @@
+import docx
+from docx.shared import Pt
+from docx.shared import Inches
+import os
+
+from tkinter import *
+from tkinter import filedialog
+import tkinter as tk 
+
+import win32com.client
+
+import string
+import time
+import sys
+sys.setrecursionlimit(20000)
+
+
+
+defaultLoc=os.getcwd()
+root=tk.Tk()
+root.title("Hand Writing generator")
+
+root.config(background="#7DCEA0")
+root.attributes('-fullscreen',True)
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+def stop():
+    root.destroy()
+def select():
+    gg=filedialog.askopenfilename(initialdir=defaultLoc,title="choose a docx file",filetypes=(("docx files","*docx")   ,("docx files","*docx")))
+    message = tk.Label(root, text="" ,bg="#7DCEA0"  ,fg="#7DCEA0"  ,width=int((0.0462962)*screen_height)  ,height=3,font=('caliber', int((0.027777)*screen_height), 'bold')) 
+    message.place(x=(0.447916*screen_width), y=(0.64814814*screen_height))
+    
+    doc = docx.Document(gg)
+    doc1=docx.Document()
+    temp=gg
+    listt=list(temp.split('/'))
+    name=listt[-1]
+    nname=list(name.split('.'))
+    name=nname[0]
+    
+    
+
+
+             
+    start(doc,doc1,name)
+    
+
+def start(doc,doc1,name):
+    wdFormatPDF=17
+    
+    for para in doc.paragraphs:
+        paragraph = doc1.add_paragraph()
+        run = paragraph.add_run(para.text)
+        font = run.font
+        font.name = "Yourwriting"
+        font.size = Pt(24)
+    #paragraph.paragraph_format.left_indent = Inches(2.5)
+    doc1.save('temp.docx')
+    word = win32com.client.Dispatch('Word.Application')
+    
+    #doc1.close()
+    doc = word.Documents.Open(defaultLoc+'/temp.docx')
+    doc.SaveAs(defaultLoc+'/'+name, FileFormat=wdFormatPDF)
+    doc.Close()
+    word.Quit()
+    message = tk.Label(root, text="Successfully converted to "+name+".pdf" ,bg="green"  ,fg="white"  ,width=int((0.0462962)*screen_height)  ,height=3,font=('caliber', int((0.027777)*screen_height), 'bold')) 
+    message.place(x=(0.207916*screen_width), y=(0.44814814*screen_height))
+
+    
+
+    
+    
+
+    
+
+
+root.bind("<Escape>",exit) 
+message = tk.Label(root, text="Document to Handwriting" ,bg="#5DADE2"  ,fg="white"  ,width=int((0.0462962)*screen_height)  ,height=3,font=('caliber', 30, 'bold')) 
+message.place(x=(0.20916*screen_width),y=(0.0185185)*screen_height)
+space = tk.Button(root, text="click to start", command=select  ,fg="white"  ,bg="#2E86C1"  ,width=int((0.0185185)*screen_height)  ,height=3, activebackground = "white" ,font=('times', 15, ' bold '))
+space.place(x=(0.447916*screen_width), y=(0.44814814*screen_height))
+close=tk.Button(root,text="X",command=stop,fg="white"  ,bg="red"  ,width=4  ,height=2, activebackground = "white" ,font=('times', 10, ' bold '))
+close.place(x=(0.963541*screen_width),y=(0.00925925*screen_height))
+root.mainloop()
+
+
+
+
